5c9e7c04c7ed80fc5a2e3360f6aa31a7cc16426c
Vinay Nayak <nayakvinay28@gmail.com>
2026-06-14 22:36:01 +0200

    Add missing terraform variables and outputs files

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+﻿output "app_url" {
+  value = "https://"
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+﻿variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "monitoring-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "prefix" {
+  description = "Prefix for all resources"
+  type        = string
+  default     = "monitoring"
+}
+
+variable "acr_name" {
+  description = "Azure Container Registry name"
+  type        = string
+  default     = "shiyomonitoring001"
+}
+
+variable "alert_email" {
+  description = "Email address for alerts"
+  type        = string
+  default     = "nayakvinay28@gmail.com"
+}
+
+variable "tags" {
+  description = "Tags for resources"
+  type        = map(string)
+  default = {
+    Environment = "dev"
+    Project     = "monitoring"
+    ManagedBy   = "terraform"
+  }
+}
